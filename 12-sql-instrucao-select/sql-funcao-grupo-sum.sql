3423324389ea8f7946c1def83499f67278ee3e99
Priscila Resende Nascimento <priscilaresende@queenstech.com.br>
2023-04-03 22:57:17 -0300

    SQL: Função de grupo SUM.

--- 12-sql-instrucao-select/sql-funcao-grupo-sum.sql
+++ 12-sql-instrucao-select/sql-funcao-grupo-sum.sql
@@ -4,5 +4,9 @@
 --------------------------- FUNÇÕES DE GRUPO -----------------------------------
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-
- 
+SELECT COUNT(*) AS QTD_PROFESSORES,
+       VL_SALARIO AS VL_SALARIO_INDIVIDUAL, 
+       SUM(VL_SALARIO) AS TOTAL_SALARIO, 
+       AVG(VL_SALARIO) AS MEDIA_SALARIO
+  FROM PROFESSOR
+ GROUP BY VL_SALARIO;
