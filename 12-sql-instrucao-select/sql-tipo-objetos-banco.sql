655cc345951dd36122d3bb9e3470974d8bf85cee
Priscila Resende Nascimento <priscilaresende@queenstech.com.br>
2023-04-07 00:36:15 -0300

    SQL: Tipos de estrutura de dados/objetos de banco.

---
+++ 12-sql-instrucao-select/sql-tipo-objetos-banco.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-------------------- TIPOS DE OBJETOS DE BANCO DE DADOS ------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+/**
+
+-- TIPOS DE OBJETOS:
+
+1) TABELA: É A UNIDADE DE ARMAZENAMENTO DOS DADOS;
+2) VIEW: REPRESENTAÇÃO LÓGICA DE UM CONJUNTO DE DADOS QUE ENVOLVEM UMA OU MAIS 
+   TABELAS;
+3) SEQUENCE: PARA GERAR VALORES NUMÉRICOS INCREMENTAIS;
+4) SINÔNIMO: FORMA DE DEFINIR NOMES ALTERNATIVOS PARA AS TABELAS;
+5) INDEX: AJUDA A MELHORAR A PERFORMANCE DAS NOSSAS CONSULTA;
+
+-- REGRAS DE NOMENCLATURA:
+
+1) TODOS OBJETO DEVE INICIAR UMA LETRA;
+2) MÁXIMO DE CARACTER PERMITIDO NA NOMENCLATURA DO OBJETO É 30 CARACTERES;
+3) OS CARACTERES PERMITIDOS SÃO: A-Z, a-z, 0-9, _, $ E #;
+4) NOMES DE OBJETOS NÃO PODEM SE REPETIR, DEVEM SER ÚNICOS;
+5) NÃO PODEMOS UTILIZAR PALAVRAS RESERVADAS DO ORACLE, COMO POR EXEMPLO: TABLE,
+   CONSTRAINT, MODIFY E ETC.
+   
+**/
