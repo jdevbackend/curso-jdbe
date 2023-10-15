2c9befd2e19f1503055f72b17381287a354bc86c
JDBE <desenvolvimento@devbackend.com.br>
2023-10-15 00:39:47 -0300

    Sintaxe Java: Algoritmo complemento de 2.

--- 13-jdbe-sintaxe-java/src/TiposPrimitivosInteiros.java
+++ 13-jdbe-sintaxe-java/src/TiposPrimitivosInteiros.java
@@ -33,5 +33,26 @@ public class TiposPrimitivosInteiros {
 
         System.out.println("Antes da operação tipo short: " + shortMenor + " === " + shortMaior);
 
+        /**
+         *
+         * Representação do número 5 positivo em binário: 00000101
+
+         * Complemento de 1 -> 11111010
+         * Complemento de 2 -> 11111011
+
+         * Representação do número 5 Negativo em binário: 11111011
+
+         * Bit mais relevante à esquerda quando 0 representa positivo
+         * Bit mais relevante à direita quando 1 representa Negativo
+         *
+         **/
+
+        int cincoPositivo = 5;
+        int cincoNegativo = -5;
+
+        int substracao = cincoPositivo - cincoNegativo;
+        int multiplicacao = cincoPositivo * cincoNegativo;
+
+        System.out.println("Resultado Sub: " + substracao + "   Resultado multiplicação: " + multiplicacao);
     }
 }
